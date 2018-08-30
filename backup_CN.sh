93b6e693cc8d59685db618fc2db6d5178cfcbe04
UXH <24637370+uxh@users.noreply.github.com>
2018-08-30 19:49:32 +0800

    Version 1.0

--- backup_CN.sh
+++ backup_CN.sh
@@ -2,27 +2,27 @@
 PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin"
 export PATH
 
-#Github：https://github.com/uxh/qcloud-cos-autobackup
-#Author：https://www.banwagongzw.com & https://www.vultrcn.com
-#Thanks：https://zhangge.net
+#托管：https://github.com/uxh/qcloud-cos-autobackup
+#作者：https://www.banwagongzw.com & https://www.vultrcn.com
+#致谢：https://zhangge.net
 
-#Set color
+#设置输出颜色
 red="\033[0;31m"
 green="\033[0;32m"
 yellow="\033[0;33m"
 plain="\033[0m"
 
-#Qcloud API setting
-apisecretid="x2L3EQpp63QLTdEUXpRkkRZmN8dkL3kpK8QG" #needed
-apisecretkey="F9Ja7SbIYRYYdaCD9PyA8UZUvqcc1DG4" #needed
+#腾讯云API密钥
+apisecretid="x2L3EQpp63QLTdEUXpRkkRZmN8dkL3kpK8QG" #需要修改
+apisecretkey="F9Ja7SbIYRYYdaCD9PyA8UZUvqcc1DG4" #需要修改
 
-#Qcloud COS setting
-cosregion="ap-shanghai" #needed
-cosbucket="example-1234567890" #needed
+#腾讯云COS配置
+cosregion="ap-shanghai" #需要修改
+cosbucket="example-1234567890" #需要修改
 
-#Zip setting
-zippassword="1234567890" #needed
-zipautodelete="true" #needed
+#压缩包配置
+zippassword="1234567890" #需要修改
+zipautodelete="true" #需要修改
 
 Bashdir=$(cd `dirname $0`; pwd)
 Date=$(date +%u)
@@ -31,10 +31,10 @@ PYTHON=$(which python)
 MYSQLDUMP=$(which mysqldump)
 
 function help_info() {
-    echo -e "[${yellow}Usage${plain}]："
-    echo -e "[${green}Backup web directory${plain}]"
+    echo -e "[${yellow}用法示例${plain}]："
+    echo -e "[${green}备份网站目录${plain}]"
     echo -e " bash backup.sh --file 123.com /home/wwwroot/123.com /home/wwwbackups/123.com"
-    echo -e "[${green}Backup web database${plain}]"
+    echo -e "[${green}备份数据库${plain}]"
     echo -e " bash backup.sh --db 123.com 123.com_database root 123456 /home/wwwbackups/123.com"
 }
 
@@ -44,12 +44,12 @@ function upload_to_cos() {
 
     ${PYTHON} ${Bashdir}/cos.upload.py ${apisecretid} ${apisecretkey} ${cosregion} ${cosbucket} ${localfile} ${cosfile}
     if [ $? -eq 0 ]; then
-        echo -e "[${green}INFO${plain}] Backup ${localfile} upload to Qcloud COS success"
+        echo -e "[${green}提示${plain}] 备份文件 ${localfile} 上传腾讯云成功"
         if [ "${zipautodelete}" == "true" ]; then
-            rm -f ${localfile} && echo -e "[${green}INFO${plain}] Backup ${localfile} has been deleted"
+            rm -f ${localfile} && echo -e "[${green}提示${plain}] 本地备份文件 ${localfile} 已删除"
         fi
     else
-        echo -e "[${red}ERROR${plain}] Backup ${localfile} upload to Qcloud COS failed, please try again!"
+        echo -e "[${red}错误${plain}] 备份文件 ${localfile} 上传腾讯云失败，请重试！"
     fi
 }
 
@@ -66,9 +66,9 @@ function backup_file() {
     fi
     ${ZIP} -P${zippassword} -9r ${backupdir}/${domain}\_${Date}\.zip ${websitedir}
     if [ $? -eq 0 ]; then
-        echo -e "[${green}INFO${plain}] Zip web directory ${websitedir} sucdcess"
+        echo -e "[${green}提示${plain}] 网站目录 ${websitedir} 压缩成功"
     else
-        echo -e "[${red}ERROR${plain}] Zip web directory ${websitedir} failed, please try again!"
+        echo -e "[${red}错误${plain}] 网站目录 ${websitedir} 压缩失败，请重试！"
     fi
 
     upload_to_cos ${backupdir}/${domain}\_${Date}\.zip ${domain}\_${Date}\.zip
@@ -87,18 +87,18 @@ function backup_db() {
     cd ${backupdir}
     ${MYSQLDUMP} -hlocalhost -u${mysqluser} -p${mysqlpassword} ${databasename} --skip-lock-tables --default-character-set=utf8 > ${backupdir}/${domain}\_db_${Date}\.sql
     if [ $? -eq 0 ]; then
-        echo -e "[${green}INFO${plain}] Mysqldump database ${databasename} success"
+        echo -e "[${green}提示${plain}] 数据库 ${databasename} 导出成功"
     else
-        echo -e "[${red}ERROR${plain}] Mysqldump database ${databasename} failes, please try again!"
+        echo -e "[${red}错误${plain}] 数据库 ${databasename} 导出失败，请重试！"
     fi
     if [ -f ${backupdir}/${domain}\_db_${Date}\.zip ]; then
         rm -f ${backupdir}/${domain}\_db_${Date}\.zip
     fi
     ${ZIP} -P ${zippassword} -m ${backupdir}/${domain}\_db_${Date}\.zip ${domain}\_db_${Date}\.sql
     if [ $? -eq 0 ]; then
-        echo -e "[${green}INFO${plain}] Zip database ${databasename} success"
+        echo -e "[${green}提示${plain}] 数据库 ${databasename} 压缩成功"
     else
-        echo -e "[${red}ERROR${plain}] Zip database ${databasename} failed, please try again!"
+        echo -e "[${red}错误${plain}] 数据库 ${databasename} 压缩失败，请重试！"
     fi
 
     upload_to_cos ${backupdir}/${domain}\_db_${Date}\.zip ${domain}\_db_${Date}\.zip
